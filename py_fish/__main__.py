c0019d712c5681356b18b11fae474ab039e74197
sorfeldt <sorfeldt@student.chalmers.se>
2024-03-04 11:22:56 +0100

    Added data

--- py_fish/__main__.py
+++ py_fish/__main__.py
@@ -1,6 +1,7 @@
 from py_fish.engine import EngineApplication, calculate_consumption
-from py_fish.data import plot_power
+from py_fish.data import plot_power, load_one_day
 
 
 print(calculate_consumption(EngineApplication.DEFAULT, 60, 120))
+load_one_day("2023-10-23")
 plot_power()
