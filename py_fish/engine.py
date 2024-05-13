7ecd72b070839455b64d372c732d2d5c796fa13a
sorfeldt <sorfeldt@student.chalmers.se>
2024-05-13 15:50:01 +0200

    Fixed idle in port issue

--- py_fish/engine.py
+++ py_fish/engine.py
@@ -77,3 +77,23 @@ def calculate_power_from_consumption(
         (consumptions / LITER_PER_GALLON - idle_fuel_consumption) / bsfc,
         [0] * len(consumptions),
     )
+
+
+def idle_port_consumption_mira(engine_rating: float, profile: np.ndarray) -> float:
+    c0, c1, c2, c3 = 0.26, 8.1e-4, 0.080, -2.1e-5
+    idle_fuel_consumption = c0 + c1 * engine_rating * HP_PER_KW
+    consumption = 0
+    (rows, _) = np.shape(profile)
+    for i in range(1, rows):
+        (speed, longitude, latitude) = profile[i, [1, 2, 3]]
+        if (
+            longitude > 11.213
+            and longitude < 11.287
+            and latitude > 58.580
+            and latitude < 58.611
+            and speed < 0.1
+        ):
+            consumption = consumption + idle_fuel_consumption * LITER_PER_GALLON * (
+                profile[i, 0] - profile[i - 1, 0]
+            )
+    return consumption
